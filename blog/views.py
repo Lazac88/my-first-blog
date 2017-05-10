2d6450502ae94cc1f4107d74c09fe2e50749e98a
tuckmn1 <tuckmn1@student.op.ac.nz>
2017-05-11 11:14:52 +1200

    Modified templates to dynamically display posts from database

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+	#Below returns a list called posts
+	#published_date is a field in our model. The '-' in front means reverse the order
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+	#Here we are passing a dictionary with the key of 'posts' and the value of our created list above
+
+	return render(request, 'blog/post_list.html', {'posts': posts})
